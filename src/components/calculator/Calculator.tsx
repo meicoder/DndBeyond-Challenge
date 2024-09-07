18a2f501ccc4b8d083e7137bdf9d46769b550835
Marcos Espinoza <meicoder7@gmail.com>
2024-09-06 21:38:01 -0400

    Add components and styles for the calculator section

--- src/components/calculator/Calculator.tsx
+++ src/components/calculator/Calculator.tsx
@@ -1,6 +1,11 @@
-import classes from './calculator.module.css';
+import styles from './calculator.module.css';
 const Calculator = () => {
-    return <div className={classes.container}>Calculator</div>;
+    return (
+        <div className={styles.container}>
+            <div className={styles.numbers}>3/6</div>
+            <div className={styles.description}>Points Spent</div>
+        </div>
+    );
 };
 
 export default Calculator;
